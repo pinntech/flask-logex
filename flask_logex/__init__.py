9e1031422025569e2bfa98f809042ee86df6b534
Tim Co <timco92@gmail.com>
2017-06-16 15:53:34 -0700

    0.1.2 release

--- flask_logex/__init__.py
+++ flask_logex/__init__.py
@@ -5,7 +5,7 @@ Contains configuration options for local, development, staging and production.
 :license: All rights reserved
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 # System
 # ~~~~~~
